873bbd2c6630689370ddb59916d8d9285de62066
LadyMeMe <ladememe2012@gmail.com>
2026-06-09 22:55:49 -0700

    Completed Task4

---
+++ Task 4.py
@@ -0,0 +1,56 @@
+#String and numerical Data Type
+#Example 1
+num1=45 #45 is no fraction so it's a integer
+print(num1)
+
+num2=12.52 #the number has a fraction of .52, so it's a Float
+print(num2)
+
+#Subtract
+subtract=7-5
+print(subtract)
+
+#Or write it like this has a float because of the decimal
+lenght=34.90-23.23
+print(lenght)
+
+#multiplication
+multiply=32*3.50 #a float
+print(multiply)
+
+#Division
+Divide=126/2.00
+print(Divide)
+
+#Expressions
+expression1=5+6*2
+print(expression1)
+
+#String_Example with empty string
+#Example 1
+name="Shamea"
+fact= "I have a pink dog"
+address= "1 San Francisco St"
+empty_Str=""
+print(name)
+print(fact)
+print(address)
+print(empty_Str)
+
+#index String
+#Example 1
+word= "Hello"
+Char0=word[0]
+Char1=word[1]
+Char2=word[2]
+Char3=word[3]
+Char4=word[4]
+print(Char0)
+print(Char1)
+print(Char2)
+print(Char3)
+print(Char4)
+
+#or you can write it like this
+greeting="Hello"
+print(greeting[0] + greeting[1] + greeting[2] + greeting[3] + greeting[4])
