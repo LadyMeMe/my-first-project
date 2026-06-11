6886f878e007add0a88fd3d1cd1c75c48a3a6ac6
LadyMeMe <ladememe2012@gmail.com>
2026-06-10 21:28:28 -0700

    Add files via upload

---
+++ Task 5 control strues-if, Elif, Else, and Boolean.py
@@ -0,0 +1,37 @@
+#This is a Boolean practice about my 3 sons and mom life
+#This is the if statement example
+from collections.abc import async_generator
+
+print ("Welcome to mom life program")
+has_older_son_terence= True
+has_middle_son_antonio= True
+has_younger_son_ayden= True
+
+if has_older_son_terence:
+    print("got a job")
+if has_middle_son_antonio:
+    print("graduates from high school")
+if has_younger_son_ayden:
+    print("asking for robux again")
+
+#This is the Elif and Else practice
+    print ("Terence has a job")
+age_terence=25
+if age_terence>= 21:
+    print ("Terence is old enough to work")
+elif terence_age>=18:
+    print ("Terence is over 18 and old enough to work")
+else:
+    print ("Terence is not old enough to work")
+
+#example 2 with Else
+ayden_finished_homewrk= True
+if ayden_finished_homewrk:
+    print("Ayden can buy Robux")
+else:
+    print("Ayden can not buy Robux until homework is finished")
+    ayden_finished_homework= False
+if ayden_finished_homewrk:
+    print("Ayden cannot buy Robux")
+else:
+    print("Ayden cannot buy Robux until homework is finished")
