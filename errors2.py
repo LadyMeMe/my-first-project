0341753e47da8df2071bb3e21deabae6843e6fab
LadyMeMe <ladememe2012@gmail.com>
2026-06-17 16:06:30 -0700

    Add errors.py and errors2.py from PythonProject

---
+++ errors2.py
@@ -0,0 +1,11 @@
+# This example program is meant to demonstrate errors.
+
+
+animal = "Lion"
+animal_type = "cub"
+number_of_teeth = 16
+
+full_spec = f" This is a {animal}. It has {number_of_teeth} and it has {animal_type} teeth"
+
+print (full_spec)
+#This is a Logical Error. I had t add a f-string so it would print the variables instead of the names.
